1f90a01d12793796ab37828b91feaa502efe673a
maxwen <max.weninger@gmail.com>
2017-09-01 15:54:53 +0200

    [1/2] base: allow to swap volume buttons rotation based
    abc edit: enable by def
    Change-Id: Ia50d9d1d8a72a649dd86b99f9701b0de05252ebe

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4081,6 +4081,12 @@ public final class Settings {
         public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
 
         /**
+         * Change volume up and down handlign based on rotation
+         * @hide
+         */
+        public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
