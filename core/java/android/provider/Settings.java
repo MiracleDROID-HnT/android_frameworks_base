73b9bfded477ca2c2c42eae1043a56f6b599b85c
xyyx <xyyx@mail.ru>
2017-09-07 11:16:04 +0800

    Ability to disable dashboards conditions (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4271,6 +4271,12 @@ public final class Settings {
         public static final String FORCE_AMBIENT_FOR_MEDIA = "force_ambient_for_media";
 
         /**
+         * Disable dashboard conditions in settings
+         * @hide
+         */
+        public static final String ENABLE_CONDITIONS = "enable_conditions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
