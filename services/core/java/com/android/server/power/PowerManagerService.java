b43e273cab5a6053aa05419a7f37ac071eeea980
thecrazyskull <anaskarbila@aospa.co>
2017-09-11 10:09:44 +0800

    PowerManagerService: add a config to light up buttons only when press…
    …ed (1/2)
    
     * Right now capactive, lit hardware keys are being
       lit every time you either touch them or the screen.
    
       But some devices handle this differently on stock:
    
       Display touch => buttons not lit
       Buttons touch => buttons lit
    
    reworked with switch (extracted from omnirom/android_frameworks_base@5d1fd91) by @xyyx

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -249,6 +249,7 @@ public final class PowerManagerService extends SystemService
     private int mButtonTimeout;
     private int mButtonBrightness;
     private int mButtonBrightnessSettingDefault;
+    private boolean mButtonPressed = false;
 
     private final Object mLock = LockGuard.installNewLock(LockGuard.INDEX_POWER);
 
@@ -296,6 +297,7 @@ public final class PowerManagerService extends SystemService
     private long mLastSleepTime;
 
     // Timestamp of the last call to user activity.
+    private long mLastButtonActivityTime;
     private long mLastUserActivityTime;
     private long mLastUserActivityTimeNoChangeLights;
 
@@ -551,6 +553,9 @@ public final class PowerManagerService extends SystemService
 
     // overrule and disable brightness for buttons
     private boolean mHardwareKeysDisable = false;
+
+    // button on touch
+    private boolean mButtonBacklightOnTouchOnly;
 
     // Set of app ids that we will always respect the wake locks for.
     int[] mDeviceIdleWhitelist = new int[0];
@@ -849,6 +854,9 @@ public final class PowerManagerService extends SystemService
         resolver.registerContentObserver(Settings.System.getUriFor(
                 Settings.System.BUTTON_BACKLIGHT_TIMEOUT),
                 false, mSettingsObserver, UserHandle.USER_ALL);
+        resolver.registerContentObserver(
+                Settings.System.getUriFor(Settings.System.BUTTON_BACKLIGHT_ON_TOUCH_ONLY),
+                false, mSettingsObserver, UserHandle.USER_ALL);
         IVrManager vrManager = (IVrManager) getBinderService(Context.VR_SERVICE);
         if (vrManager != null) {
             try {
@@ -970,6 +978,9 @@ public final class PowerManagerService extends SystemService
         mScreenBrightnessSetting = Settings.System.getIntForUser(resolver,
                 Settings.System.SCREEN_BRIGHTNESS, mScreenBrightnessSettingDefault,
                 UserHandle.USER_CURRENT);
+        mButtonBacklightOnTouchOnly = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.BUTTON_BACKLIGHT_ON_TOUCH_ONLY,
+                0, UserHandle.USER_CURRENT) != 0;
 
         mHardwareKeysDisable = Settings.Secure.getIntForUser(resolver,
                 Settings.Secure.HARDWARE_KEYS_DISABLE, 0,
@@ -1424,6 +1435,10 @@ public final class PowerManagerService extends SystemService
                 }
             } else {
                 if (eventTime > mLastUserActivityTime) {
+                    mButtonPressed = event == PowerManager.USER_ACTIVITY_EVENT_BUTTON;
+                    if (mButtonBacklightOnTouchOnly && mButtonPressed) {
+                        mLastButtonActivityTime = eventTime;
+                    }
                     mLastUserActivityTime = eventTime;
                     mDirty |= DIRTY_USER_ACTIVITY;
                     if (event == PowerManager.USER_ACTIVITY_EVENT_BUTTON) {
@@ -2012,11 +2027,16 @@ public final class PowerManagerService extends SystemService
                                 } else {
                                     buttonBrightness = mButtonBrightness;
                                 }
-                                if (mButtonTimeout != 0 && now > mLastUserActivityTime + mButtonTimeout) {
+                                mLastButtonActivityTime = mButtonBacklightOnTouchOnly ?
+                                        mLastButtonActivityTime : mLastUserActivityTime;
+                                if (mButtonTimeout != 0
+                                        && now > mLastButtonActivityTime + mButtonTimeout) {
                                     mButtonsLight.setBrightness(0);
                                 } else {
-                                    if (!mProximityPositive) {
+                                if ((!mButtonBacklightOnTouchOnly || mButtonPressed) &&
+                                        !mProximityPositive) {
                                         mButtonsLight.setBrightness(buttonBrightness);
+                                        mButtonPressed = false;
                                         if (buttonBrightness != 0 && mButtonTimeout != 0) {
                                             nextTimeout = now + mButtonTimeout;
                                         }
