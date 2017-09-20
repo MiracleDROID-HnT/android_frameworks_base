abc10deda75f1981982443a167a628e7b0c1316e
maxwen <max.weninger@gmail.com>
2017-09-20 20:54:41 +0800

    [1/2] base: unlink notification volume is back -delete old
    unused volume images -add new one for notification volumes maybe some day one
     will be able to create a vector drawable for this - I am not :)
    
    base: add vector for notification volume mute
    
    thanks to Marko Man

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4203,6 +4203,13 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_DATE_FORMAT = "statusbar_clock_date_format";
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         *
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
