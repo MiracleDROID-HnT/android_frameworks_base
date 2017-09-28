60814ed9f15d787a102798b8b3c2f6843201133b
Danesh M <daneshm90@gmail.com>
2017-09-28 09:57:06 +0800

    SystemUI : Port forward statusbar brightness control
    Brings back the status bar brightness control from cm-10.2/cm-11
    
    SystemUI: improve brightness slider behavior
    Mimic the brightness slider behavior in the statusbar.
    This adds logic to make the statusbar slider also work with automatic
    brightness mode enabled and it will instead adjust the temporary automatic
    brightness overrride.
    Change-Id: I78814a785decc83df94844fcc2f489c8cbec2b65
    
    SystemUI: status bar brightness: store value as int
    Non-automatic brightness value is stored as int, not float.
    Symptom: adjust the brightness in the status bar, the brightness
             slider in the notification bar will always be set to full
    Change-Id: Icc04e7da3286e9925f8b90528c1e022b33d33e9d
    
    SystemUI: fix status bar brightness slider eating events
    In some cases it would eat the event and not lose focus to the rest of
    the system.
    Change-Id: I94d532d09ea762edf7ac037043ed023caf42c401
    
    SystemUI : fix brightness slider for automatic brightness
    BRIGHTNESS_ADJ_RESOLUTION was changed to 2048 from 100.
    Update the calculation to reflect this change.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4399,6 +4399,12 @@ public final class Settings {
                 "volume_keys_control_media_stream";
 
         /**
+         * Whether to control brightness from status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
