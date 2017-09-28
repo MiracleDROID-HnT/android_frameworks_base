a57bf4a8c382b9de00f8e6338cba8516f00a362f
maxwen <max.weninger@gmail.com>
2017-09-28 19:44:22 +0200

    FWB: all tile scroller for quickbar settings [1/2]
    allow horizontal scrolling through all tiles also
    in the quickbar
    
    SpiritCroc:
    - Allow fancy animation when scroll position is fine
    - Clip left and right, but not top or bottom
    - Adapt to AICP
    - Bring it to O
    - Fix scrollview stealing touches from brightness slider
    
    MDroid edits:- use our Sb Observer
    
    Change-Id: I10e444218070effafb1c7b8c7f0c3f70662e0602

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4804,6 +4804,12 @@ public final class Settings {
         public static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";
 
         /**
+         * Whether to enable scrolling in the small quick settings
+         * @hide
+         */
+        public static final String QS_QUICKBAR_SCROLL_ENABLED = "qs_quickbar_scroll_enabled";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -208,6 +208,7 @@ import com.android.systemui.qs.QSPanel;
 import com.android.systemui.qs.QSTileHost;
 import com.android.systemui.qs.QuickStatusBarHeader;
 import com.android.systemui.qs.car.CarQSFragment;
+import com.android.systemui.qs.QuickStatusBarHeader;
 import com.android.systemui.recents.Recents;
 import com.android.systemui.recents.ScreenPinningRequest;
 import com.android.systemui.recents.events.EventBus;
@@ -495,6 +496,8 @@ public class StatusBar extends SystemUI implements DemoMode,
     int mLinger;
     int mInitialTouchX;
     int mInitialTouchY;
+
+    private QuickStatusBarHeader mQuickStatusBarHeaderScroller;
 
     // top bar
     protected KeyguardStatusBarView mKeyguardStatusBar;
@@ -1373,6 +1376,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     mQSPanel.setBrightnessMirror(mBrightnessMirrorController);
                     mKeyguardStatusBar.setQSPanel(mQSPanel);
                     mQuickStatusBarHeader = ((QSFragment) qs).getQsHeader();
+                    mQuickStatusBarHeaderScroller = ((QSFragment) qs).getQuickStatusBarHeader();
                 }
             });
         }
@@ -5580,6 +5584,9 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     public void onClosingFinished() {
         runPostCollapseRunnables();
+        if (mQuickStatusBarHeader != null) {
+            mQuickStatusBarHeader.onClosingFinished();
+        }
         if (!isPanelFullyCollapsed()) {
             // if we set it not to be focusable when collapsing, we have to undo it when we aborted
             // the closing
@@ -6646,6 +6653,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.RECENTS_ICON_PACK),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_QUICKBAR_SCROLL_ENABLED),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -6666,6 +6676,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             setLockscreenMediaMetadata();
             trytoinflateclusters();
             updateRecentsIconPack();
+            setQSTilesScroller();
         }
     }
 
@@ -6676,6 +6687,14 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mAmbientMediaPlaying != 0 && mAmbientIndicationContainer != null) {
             ((AmbientIndicationContainer)mAmbientIndicationContainer).setIndication(mMediaMetadata);
         }
+    }
+
+    public void setQSTilesScroller() {
+
+        if (mQuickStatusBarHeaderScroller != null) {
+            mQuickStatusBarHeaderScroller.updateSettings();
+        }
+
     }
 
     public void updateBatterySettings() {
