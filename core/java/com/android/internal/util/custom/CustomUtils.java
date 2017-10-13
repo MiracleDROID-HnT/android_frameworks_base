ecefe3666fcc5c831497e0f4be86b68f4e7e26c3
Marko Man <darkobas@gmail.com>
2017-10-13 16:48:20 +0200

    PiP tile: add toast if pip mode is not avail
    Change-Id: Idfa864c7b26f7965afb233ae377b4a520da326a8
    Signed-off-by: LakshyaTaneja <taneja.lakshay5@gmail.com>

--- core/java/com/android/internal/util/custom/CustomUtils.java
+++ core/java/com/android/internal/util/custom/CustomUtils.java
@@ -16,8 +16,11 @@
 
 package com.android.internal.util.custom;
 
+import android.app.ActivityManager;
 import android.content.Intent;
 import android.hardware.input.InputManager;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.SystemClock;
@@ -27,6 +30,8 @@ import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
+
+import java.util.List;
 
 /**
  * Some custom utilities
@@ -68,4 +73,21 @@ public class CustomUtils {
             }
         }, 20);
     }
+
+    public static ActivityInfo getRunningActivityInfo(Context context) {
+        final ActivityManager am = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        final PackageManager pm = context.getPackageManager();
+
+        List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+        if (tasks != null && !tasks.isEmpty()) {
+            ActivityManager.RunningTaskInfo top = tasks.get(0);
+            try {
+                return pm.getActivityInfo(top.topActivity, 0);
+            } catch (PackageManager.NameNotFoundException e) {
+            }
+        }
+        return null;
+    }
 }
+
