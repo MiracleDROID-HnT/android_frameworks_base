cb319b8adc3f1d63ba87849addb7934628536008
Lakshya Taneja <taneja.lakshay5@gmail.com>
2018-01-18 12:14:46 +0530

    Clear derps

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3942,7 +3942,6 @@ public final class Settings {
          */
 
         /**
-<<<<<<< HEAD
           * Volume keys control cursor in text fields (default is 0)
           * 0 - Disabled
           * 1 - Volume up/down moves cursor left/right
