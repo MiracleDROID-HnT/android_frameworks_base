00e5cab0f87bb50a2e5b6057b36d85665081e3d1
LakshyaTaneja <taneja.lakshay5@gmail.com>
2018-01-19 08:27:59 +0000

    Add Network Traffic Indicator [1/2] Taken from PureNexus and adapted to Elixir

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4061,6 +4061,18 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_ANYWHERE = "double_tap_sleep_anywhere";
 
         /**
+         * Wheter to show network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+        /**
+         * Network traffic inactivity threshold (default is 1 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
