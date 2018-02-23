204094ceb5496589d61af53c87d6e6f521fe6246
LakshyaTaneja <taneja.lakshay5@gmail.com>
2018-02-23 15:45:16 +0000

    SystemUI: SIM PIN locked view can't pop up after removed one SIM
    RootCase:SimPinView is unlocked by incorrect subId
    
    Change-Id: I149b90c8203cba61d3a8cd17e6d7635248e445ec
    CRs-Fixed: 2155094
    
    Signed-off-by: xyyx <xyyx@mail.ru>
    Signed-off-by: LakshyaTaneja <taneja.lakshay5@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6345,6 +6345,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private boolean isHwKeysDisabled() {
         return mKeyHandler != null ? mKeyHandler.isHwKeysDisabled() : false;
+    }
 
     // Assume this is called from the Handler thread.
     private void takeScreenrecord(final int mode) {
