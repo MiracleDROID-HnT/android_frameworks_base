61360170a66927bbe013507396dbb25914d02013
LakshyaTaneja <taneja.lakshay5@gmail.com>
2018-03-07 09:02:12 +0000

    Add membar to recents[1/1]
    Signed-off-by: LakshyaTaneja <taneja.lakshay5@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4325,6 +4325,12 @@ public final class Settings {
         public static final String QS_TILE_TITLE_VISIBILITY = "qs_tile_title_visibility";
 
         /**
+         * show the membar in recents
+         *  @hide
+         */
+        public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
