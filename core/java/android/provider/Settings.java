d27efd6b63cf9743d97e4bc2d69fce8df6528dcb
Nico60 <ndhouailly@gmail.com>
2018-03-18 16:53:35 +0100

    Quick settings panel : Set transparency [1/2]
    Change-Id: I309eeb1bfca6dc7db1bacd33c86199f422aa87b0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4463,6 +4463,12 @@ public final class Settings {
         public static final String FAST_CHARGING_LED_ENABLED = "fast_charging_led_enabled";
 
         /**
+         * Whether to change the transparency of the qs panel
+         * @hide
+         */
+        public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
