0b6b86929074e93add622de6cf9b947c31466eea
LakshyaTaneja <taneja.lakshay5@gmail.com>
2018-04-05 17:31:23 +0000

    Revert "SystemUI: Support recents task locking"
    This reverts commit 55d03f30ee95e3fc9dd8172392f0239b01c776ff.
    
    Signed-off-by: LakshyaTaneja <taneja.lakshay5@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
@@ -1925,16 +1925,8 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
     public final void onBusEvent(final DismissAllTaskViewsEvent event) {
         // Keep track of the tasks which will have their data removed
         ArrayList<Task> tasks = new ArrayList<>(mStack.getStackTasks());
-        ArrayList<TaskView> deletedTasks = new ArrayList<>();
-        ArrayList<TaskView> taskViews = new ArrayList<>(getTaskViews());
-        for (TaskView t : taskViews) {
-            if (Recents.sLockedTasks.contains(t.getTask())) {
-                deletedTasks.add(t);
-            }
-        }
-        taskViews.removeAll(deletedTasks);
         mAnimationHelper.startDeleteAllTasksAnimation(
-                taskViews, useGridLayout(), event.getAnimationTrigger());
+                getTaskViews(), useGridLayout(), event.getAnimationTrigger());
         event.addPostAnimationCallback(new Runnable() {
             @Override
             public void run() {
@@ -1945,9 +1937,7 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
                 // Remove all tasks and delete the task data for all tasks
                 mStack.removeAllTasks(true /* notifyStackChanges */);
                 for (int i = tasks.size() - 1; i >= 0; i--) {
-                    Task t = tasks.get(i);
-                    if (Recents.sLockedTasks.contains(t)) continue;
-                    EventBus.getDefault().send(new DeleteTaskDataEvent(t));
+                    EventBus.getDefault().send(new DeleteTaskDataEvent(tasks.get(i)));
                 }
 
                 MetricsLogger.action(getContext(), MetricsEvent.OVERVIEW_DISMISS_ALL);
