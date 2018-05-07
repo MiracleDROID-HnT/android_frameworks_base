44ecb18bc3ab30d8afa2fd1af2d97a2d8e63187e
ezio84 <brabus84@gmail.com>
2018-05-07 19:25:51 +0200

    Battery icon: fix pct disappearing when switching off power saving
    and some code cleanup
    
    Change-Id: Ic8cb3178b4e170ba984e718b4c1af25d7da248de
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -150,7 +150,7 @@ public class BatteryMeterView extends LinearLayout implements
     }
 
     public void setForceShowPercent(boolean show) {
-        mForceShowPercent = show || mPowerSave;
+        mForceShowPercent = show;
         updateShowPercent();
     }
 
@@ -202,10 +202,13 @@ public class BatteryMeterView extends LinearLayout implements
             setForceShowPercent(pluggedIn);
             // mDrawable.setCharging(pluggedIn) will invalidate the view
         }
-        mCharging = pluggedIn;
         mDrawable.setBatteryLevel(level);
         mDrawable.setCharging(pluggedIn);
         mLevel = level;
+        if (mCharging != pluggedIn) {
+            mCharging = pluggedIn;
+            updateShowPercent();
+        }
         updatePercentText();
         setContentDescription(
                 getContext().getString(charging ? R.string.accessibility_battery_level_charging
@@ -228,7 +231,6 @@ public class BatteryMeterView extends LinearLayout implements
     public void onPowerSaveChanged(boolean isPowerSave) {
         mDrawable.setPowerSave(isPowerSave);
         mPowerSave = isPowerSave;
-        mForceShowPercent = isPowerSave;
         updateShowPercent();
     }
 
@@ -254,7 +256,7 @@ public class BatteryMeterView extends LinearLayout implements
     private void updateShowPercent() {
         final boolean showing = mBatteryPercentView != null;
         if (forcePercentageQsHeader()
-                || (mStyle != BatteryMeterDrawableBase.BATTERY_STYLE_HIDDEN && ((mShowPercentText == 1) || mForceShowPercent))) {
+                || (mStyle != BatteryMeterDrawableBase.BATTERY_STYLE_HIDDEN && ((mShowPercentText == 1) || mForceShowPercent || mPowerSave || mCharging))) {
             if (!showing) {
                 mBatteryPercentView = loadPercentView();
                 if (mTextColor != 0) mBatteryPercentView.setTextColor(mTextColor);
@@ -387,9 +389,7 @@ public class BatteryMeterView extends LinearLayout implements
         }
 
         if (forcePercentageQsHeader()
-                || style == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT
-                || ((isCircleBattery() || style == BatteryMeterDrawableBase.BATTERY_STYLE_PORTRAIT
-                || style == BatteryMeterDrawableBase.BATTERY_STYLE_LANDSCAPE) && (mCharging || mPowerSave))) {
+                || style == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT) {
             mForceShowPercent = true;
         } else {
             mForceShowPercent = false;
