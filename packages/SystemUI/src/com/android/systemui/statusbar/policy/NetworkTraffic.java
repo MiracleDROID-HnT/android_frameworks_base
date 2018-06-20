12991da297b23998c24b81e7ff92080208b11e9c
Alex <alex@dirtyunicorns.com>
2018-06-20 06:44:39 -0500

    Fix janky traffic indicator tint
    This is best explained by looking at the jank
    
    https://i.imgur.com/EajdLei.png
    
    Change-Id: I8d4df1aab769755a939dcba6571cb5f0e09b243d

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
@@ -352,7 +352,7 @@ public class NetworkTraffic extends TextView {
 
     private void updateTrafficDrawableColor() {
         if (mDrawable != null) {
-            mDrawable.setColorFilter(mIconTint, PorterDuff.Mode.SRC_ATOP);
+            mDrawable.setColorFilter(mIconTint, PorterDuff.Mode.MULTIPLY);
         }
     }
 }
