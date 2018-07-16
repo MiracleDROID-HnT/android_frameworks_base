AICP/frameworks_base

Add ability to configure battery saver mode color (1/2)
Adapted for marshmallow by @KreAch3R

GZR:
======
Modified the original commit (disable/enable battery saver), to actually
have the ability to pick the color user wish.

AICP:
- Bring it to 8.1 by @LordClockaN

Change-Id: I3c3b25f32bbb181ac6ddd32c1891a0a59cb65a80
Signed-off-by: Prema Chand Alugu <premaca@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -391,6 +391,9 @@ public class StatusBar extends SystemUI implements DemoMode,
      * This affects the status bar UI. */
     private static final boolean FREEFORM_WINDOW_MANAGEMENT;
 
+    private static final String STATUS_BAR_BATTERY_SAVER_COLOR =
+            Settings.Secure.STATUS_BAR_BATTERY_SAVER_COLOR;
+
     /**
      * How long to wait before auto-dismissing a notification that was kept for remote input, and
      * has now sent a remote input. We auto-dismiss, because the app may not see a reason to cancel
@@ -541,6 +544,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         : null;
 
     private ScreenPinningRequest mScreenPinningRequest;
+
+    private int mBatterySaverColor;
 
     private final MetricsLogger mMetricsLogger = Dependency.get(MetricsLogger.class);
 
@@ -3608,6 +3613,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (powerSave && getBarState() == StatusBarState.SHADE) {
             mode = MODE_WARNING;
         }
+        if (mode == MODE_WARNING) {
+            transitions.setBatterySaverColor(mBatterySaverColor);
+        }
         transitions.transitionTo(mode, anim);
     }
 
@@ -6324,6 +6332,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.SYSTEM_UI_THEME),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.STATUS_BAR_BATTERY_SAVER_COLOR),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -6372,6 +6383,12 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.SYSTEM_UI_THEME))) {
                 updateTheme();
+            } else if (uri.equals(Settings.Secure.getUriFor(
+                    Settings.Secure.STATUS_BAR_BATTERY_SAVER_COLOR))) {
+                mBatterySaverColor = Settings.Secure.getIntForUser(
+                        mContext.getContentResolver(),
+                        Settings.Secure.STATUS_BAR_BATTERY_SAVER_COLOR,
+                        0xfff4511e, UserHandle.USER_CURRENT);
             }
         }
 
@@ -6386,6 +6403,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             updateQsPanelResources();
             setLockscreenMaxNotifications();
             updateFPQuickPulldown();
+            updateBatterySaverColor();
 
             // Update slim recents
             updateRecentsMode();
@@ -6425,6 +6443,11 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mQSPanel != null) {
             mQSPanel.updateResources();
         }
+    }
+
+    private void updateBatterySaverColor() {
+        mBatterySaverColor = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.STATUS_BAR_BATTERY_SAVER_COLOR, 0xfff4511e, UserHandle.USER_CURRENT);
     }
 
     protected final ContentObserver mNavbarObserver = new ContentObserver(mHandler) {

MiracleDROID-HnT/android_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -400,6 +400,9 @@ public class StatusBar extends SystemUI implements DemoMode,
      * This affects the status bar UI. */
     private static final boolean FREEFORM_WINDOW_MANAGEMENT;
 
+    private static final String STATUS_BAR_BATTERY_SAVER_COLOR =
+            Settings.Secure.STATUS_BAR_BATTERY_SAVER_COLOR;
+
     /**
      * How long to wait before auto-dismissing a notification that was kept for remote input, and
      * has now sent a remote input. We auto-dismiss, because the app may not see a reason to cancel
@@ -568,6 +571,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         : null;
 
     private ScreenPinningRequest mScreenPinningRequest;
+
+    private int mBatterySaverColor;
 
     Runnable mLongPressBrightnessChange = new Runnable() {
         @Override
@@ -3848,6 +3853,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (powerSave && getBarState() == StatusBarState.SHADE) {
             mode = MODE_POWERSAVE_WARNING;
         }
+        if (mode == MODE_POWERSAVE_WARNING) {
+            transitions.setBatterySaverColor(mBatterySaverColor);
+        }
         transitions.transitionTo(mode, anim);
     }
 
@@ -6656,6 +6664,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_QUICKBAR_SCROLL_ENABLED),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.STATUS_BAR_BATTERY_SAVER_COLOR),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -6677,6 +6688,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             trytoinflateclusters();
             updateRecentsIconPack();
             setQSTilesScroller();
+            updateBatterySaverColor();
         }
     }
 
@@ -6732,6 +6744,11 @@ public class StatusBar extends SystemUI implements DemoMode,
                     Settings.System.HEADS_UP_BLACKLIST_VALUES);
         splitAndAddToArrayList(mBlacklist, blackString, "\\|");
 
+    }
+
+    private void updateBatterySaverColor() {
+        mBatterySaverColor = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.STATUS_BAR_BATTERY_SAVER_COLOR, 0xfff4511e, UserHandle.USER_CURRENT);
     }
 
     private void setBrightnessSlider() {