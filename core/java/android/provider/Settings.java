accd159171cc33ba74cb45f2ce43293ecba525bd
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2018-07-27 14:09:43 +0300

    Revert "Allow to disable the Wallpaper tint [1/2]"
    This reverts commit 94f0b6fcdceba6f79f634a36f81ce122beaf426d.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4733,30 +4733,6 @@ public final class Settings {
         public static final String MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD =
                 "mute_annoying_notifications_threshold";
 
-        /**
-         * Whether to display the wallpaper tint on the power menu
-         * @hide
-         */
-        public static final String WALLPAPER_POWER_MENU_TINT = "wallpaper_power_menu_tint";
-
-        /**
-         * Whether to display the wallpaper tint in recents
-         * @hide
-         */
-        public static final String WALLPAPER_RECENTS_TINT = "wallpaper_recents_tint";
-
-        /**
-         * Whether to display the wallpaper tint in the notification shade
-         * @hide
-         */
-        public static final String WALLPAPER_NOTIFICATION_SHADE_WALLPAPER_TINT = "wallpaper_notification_shade_wallpaper_tint";
-
-        /**
-         * Whether to display the wallpaper tint in the keyguard
-         * @hide
-         */
-        public static final String WALLPAPER_KEYGUARD_WALLPAPER_TINT = "wallpaper_keyguard_wallpaper_tint";
-
         /** Unlock keystore with fingerprint after reboot
          * @hide
          */
--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -1777,9 +1777,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
                     .setUpdateListener(animation -> {
                         int alpha = (int) ((Float) animation.getAnimatedValue()
                                 * ScrimController.GRADIENT_SCRIM_ALPHA * 255);
-                        int transparent = (int) ((Float) animation.getAnimatedValue()
-                                * ScrimController.CUSTOM_GRADIENT_SCRIM_ALPHA * 255);
-                        mGradientDrawable.setAlpha(showWallpaperTint(mContext) ? alpha : transparent);
+                        mGradientDrawable.setAlpha(alpha);
                     })
                     .withEndAction(() -> getWindow().getDecorView().requestAccessibilityFocus())
                     .start();
@@ -1798,9 +1796,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
                     .setUpdateListener(animation -> {
                         int alpha = (int) ((1f - (Float) animation.getAnimatedValue())
                                 * ScrimController.GRADIENT_SCRIM_ALPHA * 255);
-                        int transparent = (int) ((1f - (Float) animation.getAnimatedValue())
-                                * ScrimController.CUSTOM_GRADIENT_SCRIM_ALPHA * 255);
-                        mGradientDrawable.setAlpha(showWallpaperTint(mContext) ? alpha : transparent);
+                        mGradientDrawable.setAlpha(alpha);
                     })
                     .start();
         }
@@ -1842,13 +1838,8 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
         }
     }
 
-    private void checkSettings() {
+   private void checkSettings() {
         mScreenshotDelay = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.SCREENSHOT_DELAY, 1500);
-    }
-
-    private static boolean showWallpaperTint(Context context) {
-        return Settings.System.getIntForUser(context.getContentResolver(),
-                Settings.System.WALLPAPER_POWER_MENU_TINT, 1, UserHandle.USER_CURRENT) == 1;
     }
 }
