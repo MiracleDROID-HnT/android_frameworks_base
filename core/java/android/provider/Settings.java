5029c5a74d1d79fa99c74ea5ed7aecbc178949fe
Kyryljan Serhij <kyryljan.serhij@gmail.com>
2019-01-25 01:15:45 +0200

    Revert "PM: Signature spoofing [1/2]"
    This reverts commit f3be6cc7050bf7cdb82f325b961654267deb0610.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11692,14 +11692,6 @@ public final class Settings {
         public static final String LOCKSCREEN_ENABLE_POWER_MENU = "lockscreen_enable_power_menu";
 
         /**
-         * Whether applications can fake a signature.
-         * 1 = permit apps to fake signature
-         * 0 = disable this feature
-         * @hide
-         */
-        public static final String ALLOW_SIGNATURE_FAKE = "allow_signature_fake";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
