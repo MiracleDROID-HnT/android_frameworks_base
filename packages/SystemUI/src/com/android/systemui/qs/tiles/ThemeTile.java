venkysai/android_frameworks_base-2

Update the themes QS tile
- Update accent drawable
- Give styles its own icon
- Give styles options their own icon as well
- Simpify some of the code while we're at it
- Change strings, old ones didn't make much sense

Change-Id: I2ba225e691f012078f5fd08775da35ec4f722a38

packages/SystemUI/src/com/android/systemui/qs/tiles/ThemeTile.java
@@ -55,8 +55,6 @@ import java.util.List;
 
 public class ThemeTile extends QSTileImpl<BooleanState> {
 
-    private final String SUBS_PACKAGE = "projekt.substratum";
-
     static final List<ThemeTileItem> sThemeItems = new ArrayList<ThemeTileItem>();
     static {
         sThemeItems.add(new ThemeTileItem(0, R.color.quick_settings_theme_tile_default,
@@ -203,11 +201,9 @@ public class ThemeTile extends QSTileImpl<BooleanState> {
 
         @Override
         public CharSequence getTitle() {
-            if (mMode == Mode.ACCENT) {
-                return mContext.getString(R.string.quick_settings_theme_tile_accent_detail_title);
-            } else {
-                return mContext.getString(R.string.quick_settings_theme_tile_style_detail_title);
-            }
+            return mContext.getString(mMode == Mode.ACCENT ?
+                    R.string.quick_settings_theme_tile_accent_detail_title :
+                    R.string.quick_settings_theme_tile_style_detail_title);
         }
 
         @Override
@@ -268,7 +264,7 @@ public class ThemeTile extends QSTileImpl<BooleanState> {
                 Item item = new Item();
                 item.tag = styleItem;
                 item.doDisableFocus = true;
-                item.iconResId = R.drawable.ic_qs_accent;
+                item.iconResId = R.drawable.ic_qs_style_list;
                 item.line1 = styleItem.getLabel(mContext);
                 items.add(item);
             }
@@ -350,7 +346,8 @@ public class ThemeTile extends QSTileImpl<BooleanState> {
     protected void handleUpdateState(BooleanState state, Object arg) {
         state.label = mContext.getString(mMode == Mode.ACCENT
                 ? R.string.quick_settings_theme_tile_title : R.string.system_theme_style_title);
-        state.icon = ResourceIcon.get(R.drawable.ic_qs_accent);
+        state.icon = ResourceIcon.get(mMode == Mode.ACCENT
+                ? R.drawable.ic_qs_accent : R.drawable.ic_qs_style);
     }
 
     @Override
@@ -360,6 +357,7 @@ public class ThemeTile extends QSTileImpl<BooleanState> {
 
     @Override
     public boolean isAvailable() {
+        String SUBS_PACKAGE = "projekt.substratum";
         return !Utils.isPackageInstalled(mContext, SUBS_PACKAGE);
     }
 

MiracleDROID-HnT/android_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/ThemeTile.java
@@ -211,11 +211,9 @@ public class ThemeTile extends QSTileImpl<BooleanState> {
 
         @Override
         public CharSequence getTitle() {
-            if (mMode == Mode.ACCENT) {
-                return mContext.getString(R.string.quick_settings_theme_tile_accent_detail_title);
-            } else {
-                return mContext.getString(R.string.quick_settings_theme_tile_style_detail_title);
-            }
+            return mContext.getString(mMode == Mode.ACCENT ?
+                    R.string.quick_settings_theme_tile_accent_detail_title :
+                    R.string.quick_settings_theme_tile_style_detail_title);
         }
 
         @Override
@@ -276,7 +274,7 @@ public class ThemeTile extends QSTileImpl<BooleanState> {
                 Item item = new Item();
                 item.tag = styleItem;
                 item.doDisableFocus = true;
-                item.icon = R.drawable.ic_qs_accent;
+                item.icon = R.drawable.ic_qs_style_list;
                 item.line1 = styleItem.getLabel(mContext);
                 items.add(item);
             }
@@ -384,7 +382,8 @@ public class ThemeTile extends QSTileImpl<BooleanState> {
     protected void handleUpdateState(BooleanState state, Object arg) {
         state.label = mContext.getString(mMode == Mode.ACCENT
                 ? R.string.quick_settings_theme_tile_title : R.string.systemui_theme_style_title);
-        state.icon = ResourceIcon.get(R.drawable.ic_qs_accent);
+        state.icon = ResourceIcon.get(mMode == Mode.ACCENT
+                ? R.drawable.ic_qs_accent : R.drawable.ic_qs_style);
     }
 
     @Override